208635958f844450baa904a8a2f133f4f72eb583
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-08-06 21:55:49 +0200

    fix eslint

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField, Typography } from '@mui/material';
+import { Box, Button, TextField } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { registerUser } from 'redux/auth/operations';
 
